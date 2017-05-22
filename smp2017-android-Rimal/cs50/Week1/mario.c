21572fc82c80e5586effbb0fd2fcfef5310d84f7
rp46 <parikhr8@gmail.com>
2017-05-22 15:44:24 +0530

    modified mario.c

--- smp2017-android-Rimal/cs50/Week1/mario.c
+++ smp2017-android-Rimal/cs50/Week1/mario.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
-main()
-{
-	int n,i,j,k;
-	printf("Enter the height of the pyramid you want:");
-	scanf("%d",&n);
-	for (int i = 0; i <= n; i++)//for height
-	{
-		for (int j = 0; j < n; j++)//for spacing
-		{
-			printf(" ");
-		}
-		for (int k = 1; k <= i ; k++)//for pyramid
-		{
-			printf("*");
-		}
-		printf("\n");
-	}
-	return 0;
+#include<stdio.h>
+#include<cs50.h>
+int main(){
+    int n,i,j;
+    printf("Enter the height of pyramid you want:");
+   n=get_int();
+    for(i=0;i<n;i++)
+    {
+        for(j=n-1;j>i;j--)
+        {
+            printf(" ");
+        }
+        for(j=0;j<=i;j++)
+        {
+            printf("*");
+        }
+        for(j=0;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
 }

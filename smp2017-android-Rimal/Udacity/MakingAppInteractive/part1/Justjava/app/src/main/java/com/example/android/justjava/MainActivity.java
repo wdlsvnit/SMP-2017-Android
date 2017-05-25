00cd780e00847f7283486a14e4e97dda2bd37897
rp46 <parikhr8@gmail.com>
2017-05-25 14:32:32 +0530

    Added making app interactive lessons

---
+++ smp2017-android-Rimal/Udacity/MakingAppInteractive/part1/Justjava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,55 @@
+/**
+ * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ *
+ * package com.example.android.justjava;
+ *
+ */
+
+package com.example.android.justjava;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+    int quantity=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void add(View view){
+        quantity = quantity + 1;
+        display(quantity);
+            }
+    public void subtract(View view){
+        quantity = quantity - 1;
+        display(quantity);
+    }
+    public void submitOrder(View view) {
+        displayPrice(quantity*10);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}

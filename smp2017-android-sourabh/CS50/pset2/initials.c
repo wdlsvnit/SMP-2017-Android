fa823532b3accc9aeb859d785b09077bbf87f737
user <user@boss>
2017-06-30 22:15:31 +0530

    Added Some Modified Files

--- smp2017-android-sourabh/CS50/pset2/initials.c
+++ smp2017-android-sourabh/CS50/pset2/initials.c
@@ -1,33 +1,24 @@
 #include<stdio.h>
-#include<cs50.h>
 #include<ctype.h>
 #include<string.h>
 
 int main(void)
 {
-    string s = get_string();
+    char s[101];
+    gets(s);
     int n =strlen(s);
     int i;
-    int temp=1;
-     
+    while(s[i++]==' '); 
+    printf("%c",s[i-1]);
     if(s!=NULL)
     {
        for(i=0;i<n;i++)
        {
-         if(temp>=1)
-         {
-          if(toupper(s[i])>='A' && toupper(s[i])<='Z')
-          {printf("%c",toupper(s[i]));
-          temp=0;
-          }  
-         }
-         
-         if(s[i]==' ')
-         temp++;
+		if(s[i]==' '&&s[i+1]!=' ')
+			printf("%c",toupper(s[i+1]));
        }
     }
     printf("\n");
-    
     return 0;
     
 }
--- smp2017-android-sourabh/Udacity/Justjava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ smp2017-android-sourabh/Udacity/Justjava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,13 +1,63 @@
 package com.example.android.justjava;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+/**
+ * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ *
+ * package com.example.android.justjava;
+ *
+ */
 
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        String message="Thank You";
+        display(quantity);
+        displayPrice(quantity*5,message);
+    }
+    public void increment(View view){
+        quantity+=1;
+        display(quantity);
+    }
+    public void decrement(View view){
+        if(quantity>0)
+            quantity-=1;
+        display(quantity);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on screen
+     */
+    private void displayPrice(int number,String message)
+    {
+        TextView priceTextview= (TextView)findViewById(R.id.price_text_view);
+        priceTextview.setText(NumberFormat.getCurrencyInstance().format(number)+"\n"+message);
+    }
+
 }

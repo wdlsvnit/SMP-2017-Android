2922e6f1bb2c87c0bbdfc7dc7d9260f5ced56845
JENISH9599 <pateljenish813@gmail.com>
2017-07-09 17:56:13 +0530

    Bugs are fixed

--- smp2017-android-jenish/FinalProjects/Tic_Tac_Toe/app/src/main/java/com/jenish9599/android/tic_tac_toe/MainActivity.java
+++ smp2017-android-jenish/FinalProjects/Tic_Tac_Toe/app/src/main/java/com/jenish9599/android/tic_tac_toe/MainActivity.java
@@ -9,33 +9,47 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
 
     int activePlayer = 0;
     int[] gameState={2,2,2,2,2,2,2,2,2} ;
     int[][] winningState={{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+    int[][] board={{2,2,2},{2,2,2},{2,2,2}};
     boolean gameOver=false;
     boolean twoPlayer=false;
+    boolean Hard=false;
+
+
+    public void esay(View view){
+        Hard = false;
+        setContentView(R.layout.oneplayer);
+
+    }
+
+    public void hard(View view){
+        Hard = true;
+        setContentView(R.layout.oneplayer);
+    }
 
     public void computer_make_move()
     {
 
-//       To dismiss the dialog
 
 
 
-
-        //find winning
-        //find to block
-        //find corner
         int movingindex = -1;
         ViewGroup temp =(ViewGroup)findViewById(R.id.girdLayout) ;
 
 
+        if(Hard)
+        {
+            movingindex = bestmove();
+        }
 
 
-        movingindex = look_for_win_or_block(1);
-
+        if(movingindex == -1)
+            movingindex = look_for_win_or_block(1);
         if(movingindex == -1)
         {
             movingindex = look_for_win_or_block(0);
@@ -53,6 +67,17 @@ public class MainActivity extends AppCompatActivity {
 
         if(movingindex != -1) {
             gameState[movingindex] = activePlayer;
+
+
+            if(movingindex <3 )
+                board[0][movingindex] = activePlayer;
+            else if (movingindex>=3 && movingindex <=5)
+                board[1][movingindex-3] = activePlayer;
+            else
+                board[2][movingindex-6] = activePlayer;
+
+
+
             String s = "" + movingindex;
             ImageView view = (ImageView) temp.findViewWithTag(s);
             view.setImageResource(R.drawable.nell);
@@ -185,6 +210,7 @@ public class MainActivity extends AppCompatActivity {
             return 6;
         if(gameState[2]==mark && gameState[4]==2 && gameState[6]==mark)
             return 4;
+//        if(gamseState[9] then == 1 )
         if(gameState[2]==2 && gameState[4]==mark && gameState[6]==mark)
             return 2;
 
@@ -196,16 +222,27 @@ public class MainActivity extends AppCompatActivity {
 
         int taggedLocation = Integer.parseInt(view.getTag().toString());
 
-        if(gameState[taggedLocation] == 2 && gameOver== false) {
+        if(gameState[taggedLocation] == 2 && !gameOver) {
 
             gameState[taggedLocation] = activePlayer;
+
+
+
+            if(taggedLocation <3 )
+            board[0][taggedLocation] = activePlayer;
+            else if (taggedLocation>=3 && taggedLocation <=5)
+                board[1][taggedLocation-3] = activePlayer;
+            else
+                board[2][taggedLocation-6] = activePlayer;
+
+
             if (activePlayer == 0) {
 
                 tappedView.setImageResource(R.drawable.xx);
                 activePlayer = 1;
 
                 result();
-                if(twoPlayer == false && gameOver==false)
+                if(!twoPlayer && !gameOver)
                 {
                     computer_make_move();
                     result();
@@ -259,27 +296,23 @@ public class MainActivity extends AppCompatActivity {
                 gameOver = true;
             }
         }
-
-        for(int i=0;i<gameState.length;i++)
-        {
-            if(gameState[i] != 2)
-            {
-                flag =true;
+        if(gameOver == false) {
+            for (int i = 0; i < gameState.length; i++) {
+                if (gameState[i] != 2) {
+                    flag = true;
+                } else {
+                    flag = false;
+                    break;
+                }
             }
-            else
-            {
-                flag = false;
-                break;
-            }
-        }
 
-        if(flag == true)
-        {
-            RelativeLayout winnerLayout = (RelativeLayout)findViewById(R.id.winnerlayout);
-            winnerLayout.setVisibility(View.VISIBLE);
-            TextView message = (TextView)findViewById(R.id.scoreboard);
-            message.setText("GAME IS DRAW");
-            gameOver = true;
+            if (flag == true) {
+                RelativeLayout winnerLayout = (RelativeLayout) findViewById(R.id.winnerlayout);
+                winnerLayout.setVisibility(View.VISIBLE);
+                TextView message = (TextView) findViewById(R.id.scoreboard);
+                message.setText("GAME IS DRAW");
+                gameOver = true;
+            }
         }
     }
 
@@ -294,6 +327,14 @@ public class MainActivity extends AppCompatActivity {
         for(int i=0;i<gameState.length;i++)
         {
             gameState[i] = 2;
+        }
+
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                board[i][j] = 2;
+            }
         }
 
         GridLayout gl = (GridLayout)findViewById(R.id.girdLayout);
@@ -318,6 +359,14 @@ public class MainActivity extends AppCompatActivity {
             gameState[i] = 2;
         }
 
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                board[i][j] = 2;
+            }
+        }
+
         GridLayout gl = (GridLayout)findViewById(R.id.girdLayout);
 
         for(int i=0;i<gl.getChildCount();i++)
@@ -325,8 +374,17 @@ public class MainActivity extends AppCompatActivity {
             ((ImageView)gl.getChildAt(i)).setImageResource(0);
         }
 
-        twoPlayer=false;
-        setContentView(R.layout.activity_main);
+        //twoPlayer=false;
+        if(twoPlayer == true)
+        {
+            twoPlayer = false;
+            setContentView(R.layout.activity_main);
+        }
+        else {
+
+            Hard = false;
+            setContentView(R.layout.gametype);
+        }
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -335,7 +393,9 @@ public class MainActivity extends AppCompatActivity {
     }
     public void oneplayer(View view)
     {
-        setContentView(R.layout.oneplayer);
+
+        setContentView(R.layout.gametype);
+
     }
 
     public void twoplayer(View view)
@@ -343,4 +403,200 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.oneplayer);
         twoPlayer=true;
     }
+
+
+    public  int bestmove()
+    {
+        int bestVal = -1000;
+
+        int row = -1;
+        int col = -1;
+
+        // Traverse all cells, evalutae minimax function for
+        // all empty cells. And return the cell with optimal
+        // value.
+        for (int i = 0; i<3; i++)
+        {
+            for (int j = 0; j<3; j++)
+            {
+                // Check if celll is empty
+                if (board[i][j]==2)
+                {
+                    // Make the move
+                    board[i][j] = 1;
+
+                    // compute evaluation function for this
+                    // move.
+                    int moveVal = minimax( 0,false);
+
+                    // Undo the move
+                    board[i][j] = 2;
+
+                    // If the value of the current move is
+                    // more than the best value, then update
+                    // best/
+                    if (moveVal > bestVal)
+                    {
+                        row = i;
+                        col = j;
+                        bestVal = moveVal;
+                    }
+                }
+            }
+        }
+
+        return (row*3) + col;
+
+    }
+
+
+
+    public int minimax(int depth, boolean isMax) {
+        int score = evaluate();
+
+        // If Maximizer has won the game return his/her
+        // evaluated score
+        if (score == 10)
+            return score;
+
+        // If Minimizer has won the game return his/her
+        // evaluated score
+        if (score == -10)
+            return score;
+
+        // If there are no more moves and no winner then
+        // it is a tie
+        if (isMovesLeft() == false)
+            return 0;
+
+        // If this maximizer's move
+        if (isMax) {
+            int best = -1000;
+
+            // Traverse all cells
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    // Check if cell is empty
+                    if (board[i][j] == 2) {
+                        // Make the move
+                        board[i][j] = 1;
+
+                        // Call minimax recursively and choose
+                        // the maximum value
+                        best = max(best,
+                                minimax(depth + 1, !isMax));
+
+                        // Undo the move
+                        board[i][j] = 2;
+                    }
+                }
+            }
+            return best;
+        }
+
+        // If this minimizer's move
+        else {
+            int best = 1000;
+
+            // Traverse all cells
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    // Check if cell is empty
+                    if (board[i][j] == 2) {
+                        // Make the move
+                        board[i][j] = 0;
+
+                        // Call minimax recursively and choose
+                        // the minimum value
+                        best = min(best,
+                                minimax(depth + 1, !isMax));
+
+                        // Undo the move
+                        board[i][j] = 2;
+                    }
+                }
+            }
+            return best;
+        }
+    }
+
+
+       public int evaluate() {
+
+
+
+            // Checking for Rows for X or O victory.
+            for (int row = 0; row<3; row++)
+            {
+                if (board[row][0]==board[row][1] &&
+                        board[row][1]==board[row][2])
+                {
+                    if (board[row][0]==1)
+                        return +10;
+                    else if (board[row][0]==0)
+                        return -10;
+                }
+            }
+
+            // Checking for Columns for X or O victory.
+            for (int col = 0; col<3; col++)
+            {
+                if (board[0][col]==board[1][col] &&
+                        board[1][col]==board[2][col])
+                {
+                    if (board[0][col]==1)
+                        return +10;
+
+                    else if (board[0][col]==0)
+                        return -10;
+                }
+            }
+
+            // Checking for Diagonals for X or O victory.
+            if (board[0][0]==board[1][1] && board[1][1]==board[2][2])
+            {
+                if (board[0][0]==1)
+                    return +10;
+                else if (board[0][0]==0)
+                    return -10;
+            }
+
+            if (board[0][2]==board[1][1] && board[1][1]==board[2][0])
+            {
+                if (board[0][2]==1)
+                    return +10;
+                else if (board[0][2]==0)
+                    return -10;
+            }
+
+            // Else if none of them have won then return 0
+            return 0;
+        }
+
+
+    public boolean isMovesLeft()
+    {
+        for (int i = 0; i<3; i++)
+            for (int j = 0; j<3; j++)
+                if (board[i][j]==2)
+                    return true;
+        return false;
+    }
+
+    public int max(int a ,int b)
+    {
+        if(a>b)
+            return a;
+        else
+            return b;
+    }
+
+    public int min(int a ,int b)
+    {
+        if(a<b)
+            return a;
+        else
+            return b;
+    }
+
 }
